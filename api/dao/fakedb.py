5c581c69770d8302d2b02ba450605f0c56ac1737
wojciechniedbala <wojciech.niedbala@nordcloud.com>
2019-03-14 22:58:00 +0100

    added fakedb

---
+++ api/dao/fakedb.py
@@ -0,0 +1,36 @@
+_FAKE_DB = {
+    'doctors': [{'id': '1', 'name': 'test'}, {'id': '2', 'name': 'test2'}],
+    'patients': []
+}
+
+def create(table_name, obj):
+    _FAKE_DB[table_name].append(obj)
+
+
+def get_all(table_name):
+    return _FAKE_DB[table_name]
+
+
+def get(table_name, id):
+    for i in _FAKE_DB[table_name]:
+        if i['id'] == id:
+            return i
+    return None
+
+
+def update(table_name, obj):
+    for ind, val in enumerate(_FAKE_DB[table_name]):
+        if val['id'] == obj.get('id'):
+            _FAKE_DB[table_name][ind] = obj
+            return True
+    return False
+            
+
+
+def delete(table_name, id):
+    for ind, val in enumerate(_FAKE_DB[table_name]):
+        if val['id'] == id:
+            del _FAKE_DB[table_name][ind]
+            return True
+    return False
+    
