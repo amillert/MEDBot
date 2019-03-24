18787b01ab1268303e07ef267c5fd618b8490627
Szpedy <romaniak7@gmail.com>
2019-03-24 11:21:12 +0100

    [Issue#18] Logic -> Models; Change Answer model; Add Marshmallow and jsonify to responses

--- api/blueprints/doctors.py
+++ api/blueprints/doctors.py
@@ -1,90 +1,74 @@
 import json
-from flask import Blueprint, request
-from api import db
+from flask import Blueprint, request, jsonify
+from api import db, ma
 from api.api_utils import json_res, query2jsonable
-from api.dao.models import User, Role, Interview
+from api.dao.models import User, Role, Interview, Question, Answer
 
 doctors_api = Blueprint('doctors', __name__)
 
 @doctors_api.route('', methods=['GET', 'POST'])
 def doctors_route():
     if request.method == 'POST':
-        req = request.get_json(force=True)
-        doctor = User(email=req['email'], password=req['password'], firstName=req['firstName'], lastName=req['lastName'], roleID=Role.get_id_by_role('Doctor'))
-        db.session.add(doctor)
-        db.session.commit()
-        return json_res({}, 201)
+        User.insert_into(request.get_json(force=True), 'Doctor')
+        return jsonify({}), 201
     else:
-        role = Role.query.filter_by(name='Doctor').first()
-        return json_res(query2jsonable(role.users), 200)
+        return jsonify({'Doctors': User.get_users_by_role('Doctor')}), 200
 
 
 @doctors_api.route('/<doctor_id>', methods=['GET', 'PUT', 'DELETE'])
 def doctor_route(doctor_id):
     if request.method == 'PUT':
         try: 
-            doctor = User.query.filter_by(id=doctor_id).update(dict(request.get_json(force=True)))
-            if doctor == 0:
-                return json_res({'error': 'Not found'}, 404)
-            db.session.commit()
-            return json_res({}, 200)
+            updated = User.update_user(request.get_json(force=True), doctor_id, 'Doctor')
+            if updated:
+                return jsonify({}), 200
+            return jsonify({'error': 'Not found'}), 404
         except Exception:
-            return json_res({'error': 'Bad request'}, 400) 
+            return jsonify({'error': 'Bad request'}), 400
     elif request.method == 'DELETE':
-        doctor = User.query.filter_by(id=doctor_id).first()
-        if doctor:
-            db.session.delete(doctor)
-            db.session.commit()
-            return json_res({}, 204)
-        else:
-            return json_res({'error': 'Not found'}, 404)
+        deleted = User.delete_user(request.get_json(force=True), doctor_id, 'Doctor')
+        if deleted:
+            return jsonify({}), 204
+        return jsonify({'error': 'Not found'}), 404
     else:
-        doctor = User.query.filter_by(id=doctor_id, roleID=Role.get_id_by_role('Doctor')).all()
+        doctor = User.get_users_by_role('Doctor', doctor_id)
         if doctor:
-            return json_res(query2jsonable(doctor), 200) 
-        else:
-            return json_res({'error': 'Not found'}, 404)
+            return jsonify({'Doctor': doctor}), 200
+        return jsonify({'error': 'Not found'}), 404
 
 
 @doctors_api.route('/<doctor_id>/interviews', methods=['GET', 'POST'])
 def doctor_route_interviews(doctor_id):
     if request.method == 'POST':
-        req = request.get_json(force=True)
-        doctor = User.query.filter_by(id=doctor_id, roleID=Role.get_id_by_role('Doctor')).first()
-        print(doctor)
-        interview = Interview(DoctorID=doctor_id, PatientID=req['PatientID'])
-        print(interview)
-        db.session.add(interview)
-        db.session.commit()
-        return json_res({}, 201)
-    else:
-        doctor = User.query.filter_by(id=doctor_id, roleID=Role.get_id_by_role('Doctor')).first()
-        if doctor:
-            interviews = doctor.sent_interviews
-            return json_res(query2jsonable(interviews), 200) 
+        inserted = Interview.insert_into(doctor_id, request.get_json(force=True))
+        if inserted:
+            return jsonify({}, 201)
         else:
-            return json_res({'error': 'Not found'}, 404)
+            return jsonify({'error': 'Bad request'}), 400
+    else:
+        print(doctor_id)
+        interviews = Interview.get_interviews_of_user(doctor_id)
+        if interviews:
+            return jsonify({'interviews': interviews}), 200
+        else:
+            return jsonify({'error': 'Not found'}), 404
       
 
 @doctors_api.route('/<doctor_id>/interviews/<interview_id>', methods=['GET', 'PUT', 'DELETE'])
 def doctor_route_interview(doctor_id, interview_id):
     if request.method == 'PUT':
         try: 
-            interview = Interview.query.filter_by(id=interview_id).update(dict(request.get_json(force=True)))
-            if interview == 0:
-                return json_res({'error': 'Not found'}, 404)
-            db.session.commit()
-            return json_res({}, 200)
+            updated = Interview.update_interview(doctor_id, interview_id, request.get_json(force=True))
+            if updated:
+                return jsonify({}), 200
+            return jsonify({'error': 'Not found'}), 404
         except Exception:
-            return json_res({'error': 'Bad request'}, 400) 
+            return jsonify({'error': 'Bad request'}), 400 
     elif request.method == 'DELETE':
-        interview = Interview.query.filter_by(id=interview_id).first()
-        if interview:
-            db.session.delete(interview)
-            db.session.commit()
-            return json_res({}, 204)
+        deleted = Interview.delete_interview(doctor_id, interview_id)
+        if deleted:
+            return jsonify({}), 204
         else:
-            return json_res({'error': 'Not found'}, 404)
+            return jsonify({'error': 'Not found'}), 404
     else:
-        interview = Interview.query.filter_by(id=interview_id, DoctorID=doctor_id)
-        return json_res(query2jsonable(interview), 200)
+        return jsonify(Interview.get_interviews_of_user(doctor_id, interview_id)), 200
--- api/blueprints/patients.py
+++ api/blueprints/patients.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request
+from flask import jsonify
 from api import db
 from api.api_utils import json_res, query2jsonable
-from api.dao.models import Role, User
+from api.dao.models import Role, User, UserSchema, RoleSchema
 
 patients_api = Blueprint('patients', __name__)
 
@@ -9,38 +10,29 @@ patients_api = Blueprint('patients', __name__)
 @patients_api.route('', methods=['GET', 'POST'])
 def patients_route():
      if request.method == 'POST':
-        req = request.get_json(force=True)
-        patient = User(email=req['email'], password=req['password'], firstName=req['firstName'], lastName=req['lastName'], roleID=Role.get_id_by_role('Patient'))
-        db.session.add(patient)
-        db.session.commit()
-        return json_res({}, 201)
+        User.insert_into(request.get_json(force=True), 'Patient')
+        return jsonify({}), 201
      else:
-        role = Role.query.filter_by(name='Patient').first()
-        return json_res(query2jsonable(role.users), 200)
+        return jsonify({'patients': User.get_users_by_role('Patient')}), 200
 
 
 @patients_api.route('/<patient_id>', methods=['GET', 'PUT', 'DELETE'])
 def patient_route(patient_id):
     if request.method == 'PUT':
         try: 
-            patient = User.query.filter_by(id=patient_id).update(dict(request.get_json(force=True)))
-            if patient == 0:
-                return json_res({'error': 'Not found'}, 404)
-            db.session.commit()
-            return json_res({}, 200)
+            updated = User.update_user(request.get_json(force=True), patient_id, 'Patient')
+            if updated:
+                return jsonify({}), 200
+            return jsonify({'error': 'Not found'}), 404
         except Exception:
-            return json_res({'error': 'Bad request'}, 400) 
+            return jsonify({'error': 'Bad request'}), 400
     elif request.method == 'DELETE':
-        patient = User.query.filter_by(id=patient_id).first()
-        if patient:
-            db.session.delete(patient)
-            db.session.commit()
-            return json_res({}, 204)
-        else:
-            return json_res({'error': 'Not found'}, 404)
+            deleted = User.delete_user(request.get_json(force=True), patient_id, 'Patient')
+            if deleted:
+                return jsonify({}), 204
+            return jsonify({'error': 'Not found'}), 404
     else:
-        patient = User.query.filter_by(id=patient_id, roleID=Role.get_id_by_role('Patient')).all()
+        patient = User.get_users_by_role('Patient', patient_id)
         if patient:
-            return json_res(query2jsonable(patient), 200) 
-        else:
-            return json_res({'error': 'Not found'}, 404)
+            return jsonify({'patient': patient}), 200
+        return jsonify({'error': 'Not found'}), 404
