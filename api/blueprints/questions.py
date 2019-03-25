240bc92f73eb8241df55d73461c4bad2714b7135
Albert Millert <amillert@roq.ad>
2019-03-25 23:39:16 +0100

    Fix questions api name

---
+++ api/blueprints/questions.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request
+from flask import jsonify
+from api import db
+from api.api_utils import json_res, query2jsonable
+from api.dao.models import Question, QuestionSchema
+
+questions_api = Blueprint('questions', __name__)
+
+
+@questions_api.route('', methods=['GET', 'POST'])
+def questions():
+     if request.method == 'POST':
+        Question.insert_into(request.get_json(force=True), 'Question')
+        return jsonify({}), 201
+     else:
+        return jsonify({'questions': Question.get_questions('Question')}), 200
