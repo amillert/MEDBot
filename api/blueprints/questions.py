3ba21e29d1291d6241238233f557d6e3ee699e88
Albert Millert <amillert@roq.ad>
2019-05-26 22:52:25 +0200

    Fixed spelling

--- api/blueprints/questions.py
+++ api/blueprints/questions.py
@@ -17,7 +17,7 @@ def questions():
          return jsonify({'questions': Question.get_all_questions()}), 200
    except IntegrityError as e:
       db.session.rollback()
-      return jsonify({'error': 'Question is alredy in the database'}), 400
+      return jsonify({'error': 'Question is already in the database'}), 400
 
 @questions_api.route('/<question_id>', methods=['DELETE', 'PUT', 'GET'])
 def question(question_id):
